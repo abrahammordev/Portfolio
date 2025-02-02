0698c7c185cea69d4b21b55b74897709a68fb955
abrahammordev <abrahammordev@gmail.com>
2025-02-02 16:58:03 +0100

    Refactor component structure and enhance Home and Contact pages layout

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,22 @@
+import { Container, SimpleGrid, Box } from "@chakra-ui/react";
 import ContactForm from "../components/ContactComponents/ContactForm/ContactForm";
 import ContactInfo from "../components/ContactComponents/ContactInfo";
 
+const Contact: React.FC = () => {
 
-const Contact : React.FC= () => {
   return (
-    <div>
-      <ContactInfo />
-      <ContactForm />
-    </div>
+    <Container maxW="container.xl" py={16}>
+      
+
+      <SimpleGrid columns={{ base: 1, md: 1 }} spacing={10}>
+        <Box>
+          <ContactInfo />
+        </Box>
+        <Box>
+          <ContactForm />
+        </Box>
+      </SimpleGrid>
+    </Container>
   );
 };
 
